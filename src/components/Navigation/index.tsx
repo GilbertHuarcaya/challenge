42679ba87e1b1ebb7782699c3660d60748ac8877
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2022-04-17 19:42:06 -0500

    created navigation component

---
+++ src/components/Navigation/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { User } from "../../interfaces/user/types.d";
+import DeleteInputIcon from "../Buttons/IconsBtn/DeleteInputIcon";
+import NotificationIcon from "../Buttons/IconsBtn/NotificationIcon";
+import SearchIcon from "../Buttons/IconsBtn/SearchIcon";
+import "./styles.scss";
+
+type NavProps = {
+  user: User;
+};
+
+function Navigation(props: NavProps) {
+  const { user } = props;
+
+  return (
+    <div className="nav">
+      <SearchIcon></SearchIcon>
+      <div className="nav__search">
+        <input
+          className="nav__search__input"
+          type="text"
+          placeholder="Search"
+        />
+        <DeleteInputIcon></DeleteInputIcon>
+      </div>
+      <NotificationIcon></NotificationIcon>
+      {user? <img
+        className="nav__userLogo avatar__logo-medium"
+        src={user.avatar}
+        alt="userLogo"
+      />: null}
+    </div>
+  );
+}
+
+export default Navigation;
