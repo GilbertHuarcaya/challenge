1520897c8f91cff5dc54e24fe6b71c3afa921ce4
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2022-04-17 19:39:20 -0500

    created task card component

---
+++ src/components/TaskCard/index.tsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { Task } from "../../interfaces/task/types.d";
+import OptionsIcon from "../Buttons/IconsBtn/OptionsIcon";
+import Tag from "../Tag";
+import AttachIcon from "../../images/icons/attach.svg";
+import BranchIcon from "../../images/icons/branch.svg";
+import MessageIcon from "../../images/icons/message.svg";
+import "./styles.scss";
+import DueDateBtn from "../Buttons/Btns/DueDateBtn";
+import CardOptions from "../Modals/CardOptions";
+import NewTask from "../Modals/NewTask";
+
+type TaskCardProps = {
+  task: Task;
+};
+
+const TaskCard = (props: TaskCardProps) => {
+  const [optionsHidden, setOptionsHidden] = useState(true);
+
+  const { task } = props;
+
+  let day: string = "";
+  if (
+    new Date().toLocaleDateString("en-GB", {
+      dateStyle: "long",
+    }) ===
+    new Date(task.dueDate).toLocaleDateString("en-GB", {
+      dateStyle: "long",
+    })
+  ) {
+    day = "TODAY";
+  } else if (new Date() > new Date(task.dueDate)) {
+    day = "YESTERDAY";
+  } else
+    day = new Date(task.dueDate).toLocaleDateString("en-GB", {
+      dateStyle: "long",
+    });
+
+  const OpenOptions = () => {
+    setOptionsHidden(!optionsHidden);
+  };
+
+  return (
+    <div className="task-card">
+      <div className="task-card__row">
+        <p className="task-card__name">{task.name}</p>
+        <button className="btn__secondary-transparent" onClick={OpenOptions}>
+          <OptionsIcon></OptionsIcon>
+        </button>
+        <CardOptions task={task} hidden={optionsHidden}></CardOptions>
+      </div>
+      <div className="task-card__row">
+        <p className="task-card__points">{task.pointEstimate} Pts</p>
+        <DueDateBtn dueDate={day}></DueDateBtn>
+      </div>
+      <div className="task-card__row">
+        <div className="task-card__tags">
+          {task.tags.map((tag: string) => (
+            <Tag tagName={tag} key={tag + task.id}></Tag>
+          ))}
+        </div>
+      </div>
+      <div className="task-card__row">
+        <img
+          className="avatar__logo-small"
+          src={task.assignee.avatar}
+          alt="asigneeLogo"
+        />
+        <div className="task-card__info__icons">
+          <img src={AttachIcon} alt="attach" />
+          <div>
+            <p>3</p>
+            <img src={BranchIcon} alt="attach" />
+          </div>
+          <div>
+            <p>3</p>
+            <img src={MessageIcon} alt="attach" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TaskCard;
