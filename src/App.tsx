32d4c54b9f40cc35739715ecc16dbfb43637d372
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2022-04-17 20:44:49 -0500

    updated routing for settings page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFoundPage from "./pages/NotFoundPage";
 import ErrorPage from "./pages/ErrorPage";
+import Settings from "./pages/Settings";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="" element={<Home />} />
+        <Route path="/settings" element={<Settings />} />
         <Route path="*" element={<NotFoundPage />} />
         <Route path="/error" element={<ErrorPage />} />
       </Routes>
